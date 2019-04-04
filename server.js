efeee79f314bd584cb130698002f067b1c84f91d
Xindi Xu <xindixu@utexas.edu>
2019-04-03 21:19:15 -0500

    better setup for heroku

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
+// const express = require('express');
+// const port = process.env.PORT || 8080;
+// const app = express();
+//
+// app.use(express.static(__dirname + '/dist/'));
+// app.get(/.*/, function(req, res) {
+//   res.sendfile(__dirname + '/dist/index.html');
+// });
+// app.listen(port);
+// console.log('serve started');
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + '/dist/index.html');
-});
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
 app.listen(port);
-console.log('serve started');
