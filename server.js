933a699b114d9719e157f4e7a7f148936728d4a3
Xindi Xu <xindixu@utexas.edu>
2019-04-03 22:03:10 -0500

    config server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// const express = require('express');
-// const port = process.env.PORT || 8080;
-// const app = express();
-//
-// app.use(express.static(__dirname + '/dist/'));
-// app.get(/.*/, function(req, res) {
-//   res.sendfile(__dirname + '/dist/index.html');
-// });
-// app.listen(port);
-// console.log('serve started');
-
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+
+// create the express app
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// Catch all routes and redirect to the index file
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port);
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port);
