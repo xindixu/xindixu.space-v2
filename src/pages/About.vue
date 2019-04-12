f6399e62356f63089682a56dfa1ad6ecb7f105a6
Xindi Xu <xindixu@utexas.edu>
2019-04-11 19:55:52 -0500

    lazy load swpiers

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,154 +2,163 @@
   <div>
     <div class="swiper-container">
       <swiper ref="swiper" :options="swiperOptionV">
-        <swiper-slide style="background-image: url('img/bg/bg29.jpg');">
-          <div class="row">
-            <div class="svg-wrapper"><Xu></Xu><Xu0></Xu0></div>
-            <div class="svg-wrapper"><Xin></Xin><Xin0></Xin0></div>
-            <div class="svg-wrapper"><Di></Di><Di0></Di0></div>
-            <div style="margin-top:130px">
-              <span @click="pronounce">
-                <SquareLink>pronounce</SquareLink>
-              </span>
+        <swiper-slide>
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg29.jpg">
+            <div class="swiper-lazy-preloader"></div>
+            <div class="row">
+              <div class="svg-wrapper"><Xu></Xu><Xu0></Xu0></div>
+              <div class="svg-wrapper"><Xin></Xin><Xin0></Xin0></div>
+              <div class="svg-wrapper"><Di></Di><Di0></Di0></div>
+              <div style="margin-top:130px">
+                <span @click="pronounce">
+                  <SquareLink>pronounce</SquareLink>
+                </span>
+              </div>
+              <audio src="audio/name.mp3" id="audio"></audio>
             </div>
-            <audio src="audio/name.mp3" id="audio"></audio>
-          </div>
-          <div class="row mt-5">
-            <div class="col-md-6 col-lg-5">
-              <p>
-                My name consists of 3 Chinese characters, family name,
-                <span class="chinese">徐</span> and given name,
-                <span class="chinese">欣笛</span>.
-                <span class="chinese">欣</span> means happiness.
-                <span class="chinese">笛</span> means bamboo flutes, whose music
-                often triggers a peaceful joy among the listeners.
-              </p>
+            <div class="row mt-5">
+              <div class="col-md-6 col-lg-5">
+                <p>
+                  My name consists of 3 Chinese characters, family name,
+                  <span class="chinese">徐</span> and given name,
+                  <span class="chinese">欣笛</span>.
+                  <span class="chinese">欣</span> means happiness.
+                  <span class="chinese">笛</span> means bamboo flutes, whose
+                  music often triggers a peaceful joy among the listeners.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="row" style="margin-top:10em">
-            <div class="col-lg-6 col-md-10 offset-lg-5">
-              <div id="map-container" class="mx-4">
-                <img id="map" src="img/about/map.png" />
-                <Pin class="label pinFZ"></Pin>
-                <Pin class="label pinAUS"></Pin>
-                <!-- <Route class="label route"></Route> -->
+            <div class="row" style="margin-top:10em">
+              <div class="col-lg-6 col-md-10 offset-lg-5">
+                <div id="map-container" class="mx-4">
+                  <img id="map" src="img/about/map.png" />
+                  <Pin class="label pinFZ"></Pin>
+                  <Pin class="label pinAUS"></Pin>
+                  <!-- <Route class="label route"></Route> -->
+                </div>
+                <div id="text-container">
+                  <SquareTexts
+                    id="tagFZ"
+                    num1="0324"
+                    num2="1998"
+                    en="Fuzhou, China"
+                    zh="中国福州"
+                  ></SquareTexts>
+                  <SquareTexts
+                    id="tagAUS"
+                    num1="0809"
+                    num2="2016"
+                    en="Austin, U.S.A."
+                    zh="美国奥斯汀"
+                  ></SquareTexts>
+                </div>
+                <div>
+                  <p>
+                    I was born in Fuzhou, China and spent my first 18 years
+                    there. I moved to the US for college. <br />
+                    Now, I am a senior studying Advertising and Computer Science
+                    at the University of Texas at Austin.
+                  </p>
+                </div>
               </div>
-              <div id="text-container">
-                <SquareTexts
-                  id="tagFZ"
-                  num1="0324"
-                  num2="1998"
-                  en="Fuzhou, China"
-                  zh="中国福州"
-                ></SquareTexts>
-                <SquareTexts
-                  id="tagAUS"
-                  num1="0809"
-                  num2="2016"
-                  en="Austin, U.S.A."
-                  zh="美国奥斯汀"
-                ></SquareTexts>
-              </div>
-              <div>
-                <p>
-                  I was born in Fuzhou, China and spent my first 18 years there.
-                  I moved to the US for college. <br />
-                  Now, I am a senior studying Advertising and Computer Science
-                  at the University of Texas at Austin.
-                </p>
-              </div>
-            </div>
-            <div class="col-lg-1 col-md-2">
-              <div class="vertical chinese">
-                <p>
-                  我生长于中国福州。三年前我来到了美国奥斯汀开启了我的大学生活。<br />
-                  现在我大四，就读于德州大学奥斯汀分校，学习广告和计算机科学。
-                </p>
-              </div>
-            </div>
-          </div>
-          <scroll-down color="#000000" :scrollTo="5000"></scroll-down>
-        </swiper-slide>
-
-        <swiper-slide style="background-image: url('img/bg/bg31.jpg');">
-          <div class="row">
-            <div class="col half">
-              <div class="white left top">
-                <h1>Learning...</h1>
-                <p>
-                  I major in advertising and I am part of TexasMedia. I just
-                  finished my certificate for Computer Science and I will keep
-                  explore more.
-                </p>
-                <p>
-                  I want to be a web developer after graduation so I am
-                  dedicated to learn lots of modern web technologies online.
-                </p>
+              <div class="col-lg-1 col-md-2">
+                <div class="vertical chinese">
+                  <p>
+                    我生长于中国福州。三年前我来到了美国奥斯汀开启了我的大学生活。<br />
+                    现在我大四，就读于德州大学奥斯汀分校，学习广告和计算机科学。
+                  </p>
+                </div>
               </div>
             </div>
-            <div class="col half chinese">
-              <div class="white right bottom">
-                <h1 class="chineseTitle">学习中...</h1>
-                <p>
-                  我主修广告，是TexasMedia的一员。我刚刚拿到了我的计算机科学的证书，我也会继续深入学习。
-                </p>
-                <p>
-                  我毕业后想做个网页开发工程师，所以全身心地学习各种现代网页开发技术。
-                </p>
-              </div>
-            </div>
+            <scroll-down color="#000000" :scrollTo="5000"></scroll-down>
           </div>
         </swiper-slide>
 
-        <swiper-slide style="background-image: url('img/bg/bg33.jpg');">
-          <div class="row">
-            <div class="col half">
-              <div class="white left bottom">
-                <h1>Working...</h1>
-                <p>
-                  I am currently interning at Nokia as a front-end web
-                  developer. My work includes designing, implementing, and
-                  maintaining websites with Sketch, WordPress, HTML, JS, and
-                  Python Django.
-                </p>
+        <swiper-slide>
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg31.jpg">
+            <div class="row">
+              <div class="col half">
+                <div class="white left top">
+                  <h1>Learning...</h1>
+                  <p>
+                    I major in advertising and I am part of TexasMedia. I just
+                    finished my certificate for Computer Science and I will keep
+                    explore more.
+                  </p>
+                  <p>
+                    I want to be a web developer after graduation so I am
+                    dedicated to learn lots of modern web technologies online.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div class="col half chinese">
-              <div class="white right top">
-                <h1 class="chineseTitle">工作中...</h1>
-                <p>
-                  我现在在Nokia实习，做前端开发。我的工作包括设计，开发，维护网站，主要用Sketch，WordPress，HTML，JS，和Python
-                  Django.
-                </p>
+              <div class="col half chinese">
+                <div class="white right bottom">
+                  <h1 class="chineseTitle">学习中...</h1>
+                  <p>
+                    我主修广告，是TexasMedia的一员。我刚刚拿到了我的计算机科学的证书，我也会继续深入学习。
+                  </p>
+                  <p>
+                    我毕业后想做个网页开发工程师，所以全身心地学习各种现代网页开发技术。
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </swiper-slide>
-        <swiper-slide style="background-image: url('img/bg/bg19.jpg');">
-          <div class="row">
-            <div class="col half">
-              <div class="white left top">
-                <h1>Enjoying...</h1>
-                <p>
-                  I love my super needy kitty, Dr. Casper Meowspy. I love to
-                  exercise, weight trainning, Tae Kwon Do, swimming, anything
-                  without "balls". During my vocations, I love to travel around
-                  and explore new places.
-                </p>
+
+        <swiper-slide>
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg33.jpg">
+            <div class="row">
+              <div class="col half">
+                <div class="white left bottom">
+                  <h1>Working...</h1>
+                  <p>
+                    I am currently interning at Nokia as a front-end web
+                    developer. My work includes designing, implementing, and
+                    maintaining websites with Sketch, WordPress, HTML, JS, and
+                    Python Django.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div class="col half chinese">
-              <div class="white right bottom">
-                <h1 class="chineseTitle">玩耍中...</h1>
-                <p>
-                  我爱我的猫猫，Dr. Casper Meowspy。
-                  我乐于锻炼身体，健身房，跆拳道，游泳，任何和“球”无关的运动。在放假时，我喜欢出游，探索各种地方。
-                </p>
+              <div class="col half chinese">
+                <div class="white right top">
+                  <h1 class="chineseTitle">工作中...</h1>
+                  <p>
+                    我现在在Nokia实习，做前端开发。我的工作包括设计，开发，维护网站，主要用Sketch，WordPress，HTML，JS，和Python
+                    Django.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div v-observe-visibility="visibilityChangedS3"></div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg19.jpg">
+            <div class="row">
+              <div class="col half">
+                <div class="white left top">
+                  <h1>Enjoying...</h1>
+                  <p>
+                    I love my super needy kitty, Dr. Casper Meowspy. I love to
+                    exercise, weight trainning, Tae Kwon Do, swimming, anything
+                    without "balls". During my vocations, I love to travel
+                    around and explore new places.
+                  </p>
+                </div>
+              </div>
+              <div class="col half chinese">
+                <div class="white right bottom">
+                  <h1 class="chineseTitle">玩耍中...</h1>
+                  <p>
+                    我爱我的猫猫，Dr. Casper Meowspy。
+                    我乐于锻炼身体，健身房，跆拳道，游泳，任何和“球”无关的运动。在放假时，我喜欢出游，探索各种地方。
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div v-observe-visibility="visibilityChangedS3"></div>
+          </div>
         </swiper-slide>
         <swiper-slide style="background-color:white">
           <transition
@@ -233,27 +242,31 @@ export default {
         keyboard: {
           enabled: true,
           onlyInViewport: false
-        }
+        },
+        lazy: {
+          loadPrevNext: true
+        },
+        preloadImages: false
       },
       words: [
         ['Angular', 40],
         ['Vue.js', 40],
         ['React', 40],
         ['JavaScript', 30],
-        ['TypeScript', 20],
+        ['TypeScript', 10],
         ['Express.js', 20],
         ['Firebase', 20],
         ['Node.js', 20],
         ['Python3', 20],
         ['Django', 20],
+        ['Flask', 20],
         ['Processing', 20],
         ['Swift', 20],
         ['MongoDB', 15],
         ['SQL', 15],
-        ['Phaser', 15],
+        ['Phaser', 10],
         ['Bootstrap', 10],
         ['Material', 10],
-        ['ElementUI', 10],
         ['Java', 10],
         ['Illustrator', 10],
         ['Photoshop', 10],
@@ -262,8 +275,8 @@ export default {
         ['AfterEffects', 10],
         ['Maya', 10],
         ['CMS', 10],
-        ['Google Analytics', 5],
-        ['Tableau', 8],
+        ['Google Analytics', 15],
+        ['Tableau', 10],
         ['Adwords', 5]
       ],
       wordShow: false,
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,181 +2,187 @@
   <div>
     <div class="swiper-container">
       <swiper id="v" :options="swiperOptionV">
-        <swiper-slide
-          id="slide1"
-          style="background-image:url('img/home/2.jpg')"
-        >
-          <div class="col-lg-5 offset-lg-7 col-md-6 offset-md-6">
-            <div class="zhIntro vertical">
-              <h1 class="chineseTitle">徐欣笛</h1>
-              <div class="chinese indentV">
-                <h4>网页开发工程师</h4>
-                <h4>疯狂广告人</h4>
-                <h4>终身学习家</h4>
+        <swiper-slide id="slide1">
+          <div class="bg-img swiper-lazy" data-background="img/home/2.jpg">
+            <div class="swiper-lazy-preloader"></div>
+            <div class="col-lg-5 offset-lg-7 col-md-6 offset-md-6">
+              <div class="zhIntro vertical">
+                <h1 class="chineseTitle">徐欣笛</h1>
+                <div class="chinese indentV">
+                  <h4>网页开发工程师</h4>
+                  <h4>疯狂广告人</h4>
+                  <h4>终身学习家</h4>
+                </div>
               </div>
-            </div>
 
-            <div class="enIntro">
-              <h1>Xindi Xu</h1>
-              <div class="english indentH">
-                <h4>Dedicated Web Developer</h4>
-                <h4>Passionate Advertiser</h4>
-                <h4>Lifelong Learner</h4>
+              <div class="enIntro">
+                <h1>Xindi Xu</h1>
+                <div class="english indentH">
+                  <h4>Dedicated Web Developer</h4>
+                  <h4>Passionate Advertiser</h4>
+                  <h4>Lifelong Learner</h4>
+                </div>
               </div>
             </div>
           </div>
           <scroll-down> </scroll-down>
         </swiper-slide>
-        <swiper-slide
-          id="slide2"
-          class="slideHelp"
-          style="background-image: url('img/bg/bg12.jpg');"
-        >
-          <div class="row">
-            <div class="col-lg-6 col-md-8">
-              <h1>Web Development</h1>
-              <div><img class="introPic" src="img/home/web.svg" alt="" /></div>
-              <h4 class="english">
-                Establishing web presence contributes a lot to the success of
-                your business.
-              </h4>
-              <h4 class="english">
-                Why not build a stunning website together?
-              </h4>
+        <swiper-slide id="slide2" class="slideHelp">
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg12.jpg">
+            <div class="swiper-lazy-preloader"></div>
+
+            <div class="row">
+              <div class="col-lg-6 col-md-8">
+                <h1>Web Development</h1>
+                <div>
+                  <img class="introPic" src="img/home/web.svg" alt="" />
+                </div>
+                <h4 class="english">
+                  Establishing web presence contributes a lot to the success of
+                  your business.
+                </h4>
+                <h4 class="english">
+                  Why not build a stunning website together?
+                </h4>
+              </div>
+
+              <div class="offset-lg-4 offset-md-2 col-lg-2 col-md-2">
+                <div class="zhIntro vertical">
+                  <h1 class="chineseTitle">网页开发</h1>
+                  <h4 class="chinese indentV">在网络上刷存在感很重要</h4>
+                  <h4 class="chinese indentV">不如让我来帮你？</h4>
+                </div>
+              </div>
             </div>
 
-            <div class="offset-lg-4 offset-md-2 col-lg-2 col-md-2">
-              <div class="zhIntro vertical">
-                <h1 class="chineseTitle">网页开发</h1>
-                <h4 class="chinese indentV">在网络上刷存在感很重要</h4>
-                <h4 class="chinese indentV">不如让我来帮你？</h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-lg-4 col-md-6 offset-lg-6 offset-md-2 project">
-              <div>
-                <img class="projectPic" src="img/home/code.svg" alt="" />
-              </div>
-              <div class="row substitle">
-                <div class="col-1">
-                  <h4>These</h4>
-                  <h4 class="chinese">这</h4>
+            <div class="row">
+              <div class="col-lg-4 col-md-6 offset-lg-6 offset-md-2 project">
+                <div>
+                  <img class="projectPic" src="img/home/code.svg" alt="" />
                 </div>
-                <div class="col-2">
-                  <IncreaseNum start="0" end="10"></IncreaseNum>
-                </div>
-                <div class="col-9">
-                  <h4>projects can prove my ability</h4>
-                  <h4 class="chinese">个项目可以证明我</h4>
-                </div>
-                <div class="link">
-                  <SquareLink link="work#development">take a look</SquareLink>
-                </div>
-              </div>
-            </div>
-          </div>
-        </swiper-slide>
-        <swiper-slide
-          class="slideHelp"
-          style="background-image: url('img/bg/bg10.jpg');"
-        >
-          <div class="row">
-            <div class="col-lg-6 col-md-8">
-              <h1>Strategic Marketing</h1>
-              <div>
-                <img class="introPic" src="img/home/analysis.svg" alt="" />
-              </div>
-              <h4 class="english">
-                Research is the cornerstone of an effective marketing strategy.
-              </h4>
-              <h4 class="english">
-                Let me help you to translate the facts and numbers to actionable
-                business decisions that make an impact.
-              </h4>
-            </div>
-
-            <div class="offset-lg-4 offset-md-2 col-lg-2 col-md-2">
-              <div class="zhIntro vertical">
-                <h1 class="chineseTitle">营销企划</h1>
-                <h4 class="chinese indentV">从市场调研中披沙沥金</h4>
-                <h4 class="chinese indentV">规划出有效的广告策略</h4>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-lg-4 col-md-6 offset-lg-6 offset-md-2 project">
-              <div>
-                <img class="projectPic" src="img/home/project.svg" alt="" />
-              </div>
-              <div class="row substitle">
-                <div class="col-1">
-                  <h4>These</h4>
-                  <h4 class="chinese">这</h4>
-                </div>
-                <div class="col-2">
-                  <IncreaseNum start="0" end="10"></IncreaseNum>
-                </div>
-                <div class="col-9">
-                  <h4>projects can prove my ability</h4>
-                  <h4 class="chinese">个项目可以证明我</h4>
-                </div>
-                <div class="link">
-                  <SquareLink link="work#marketing">take a look</SquareLink>
+                <div class="row substitle">
+                  <div class="col-1">
+                    <h4>These</h4>
+                    <h4 class="chinese">这</h4>
+                  </div>
+                  <div class="col-2">
+                    <IncreaseNum start="0" end="10"></IncreaseNum>
+                  </div>
+                  <div class="col-9">
+                    <h4>projects can prove my ability</h4>
+                    <h4 class="chinese">个项目可以证明我</h4>
+                  </div>
+                  <div class="link">
+                    <SquareLink link="work#development">take a look</SquareLink>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </swiper-slide>
-        <swiper-slide
-          class="slideHelp"
-          style="background-image: url('img/bg/bg25.jpg');"
-        >
-          <div class="row">
-            <div class="col" lg="6" md="8">
-              <h1>Creative Execution</h1>
-              <div>
-                <img class="introPic" src="img/home/visual.svg" alt="" />
+        <swiper-slide class="slideHelp">
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg10.jpg">
+            <div
+              class="swiper-lazy-preloader swiper-lazy-preloader-white"
+            ></div>
+            <div class="row">
+              <div class="col-lg-6 col-md-8">
+                <h1>Strategic Marketing</h1>
+                <div>
+                  <img class="introPic" src="img/home/analysis.svg" alt="" />
+                </div>
+                <h4 class="english">
+                  Research is the cornerstone of an effective marketing
+                  strategy.
+                </h4>
+                <h4 class="english">
+                  Let me help you to translate the facts and numbers to
+                  actionable business decisions that make an impact.
+                </h4>
               </div>
-              <h4 class="english">
-                Got an amazing idea, but don't know how to execute it?
-              </h4>
-              <h4 class="english">
-                No worries. I can help you get your idea across with compelling
-                visuals.
-              </h4>
+
+              <div class="offset-lg-4 offset-md-2 col-lg-2 col-md-2">
+                <div class="zhIntro vertical">
+                  <h1 class="chineseTitle">营销企划</h1>
+                  <h4 class="chinese indentV">从市场调研中披沙沥金</h4>
+                  <h4 class="chinese indentV">规划出有效的广告策略</h4>
+                </div>
+              </div>
             </div>
 
-            <div class="offset-lg-4 offset-md-2 col-lg-2 col-md-2">
-              <div class="zhIntro vertical">
-                <h1 class="chineseTitle">创意设计</h1>
-                <h4 class="chinese indentV">有想法却不知如何表达？</h4>
-                <h4 class="chinese indentV">让我帮你以最有趣的形式传达它</h4>
+            <div class="row">
+              <div class="col-lg-4 col-md-6 offset-lg-6 offset-md-2 project">
+                <div>
+                  <img class="projectPic" src="img/home/project.svg" alt="" />
+                </div>
+                <div class="row substitle">
+                  <div class="col-1">
+                    <h4>These</h4>
+                    <h4 class="chinese">这</h4>
+                  </div>
+                  <div class="col-2">
+                    <IncreaseNum start="0" end="10"></IncreaseNum>
+                  </div>
+                  <div class="col-9">
+                    <h4>projects can prove my ability</h4>
+                    <h4 class="chinese">个项目可以证明我</h4>
+                  </div>
+                  <div class="link">
+                    <SquareLink link="work#marketing">take a look</SquareLink>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+        </swiper-slide>
+        <swiper-slide class="slideHelp">
+          <div class="bg-img swiper-lazy" data-background="img/bg/bg25.jpg">
+            <div class="swiper-lazy-preloader"></div>
 
-          <div class="row">
-            <div class="col-lg-4 col-md-6 offset-lg-6 offset-md-2 project">
-              <div>
-                <img class="projectPic" src="img/home/graphic.svg" alt="" />
+            <div class="row">
+              <div class="col" lg="6" md="8">
+                <h1>Creative Execution</h1>
+                <div>
+                  <img class="introPic" src="img/home/visual.svg" alt="" />
+                </div>
+                <h4 class="english">
+                  Got an amazing idea, but don't know how to execute it?
+                </h4>
+                <h4 class="english">
+                  No worries. I can help you get your idea across with
+                  compelling visuals.
+                </h4>
               </div>
-              <div class="row substitle">
-                <div class="col-1">
-                  <h4>These</h4>
-                  <h4 class="chinese">这</h4>
+
+              <div class="offset-lg-4 offset-md-2 col-lg-2 col-md-2">
+                <div class="zhIntro vertical">
+                  <h1 class="chineseTitle">创意设计</h1>
+                  <h4 class="chinese indentV">有想法却不知如何表达？</h4>
+                  <h4 class="chinese indentV">让我帮你以最有趣的形式传达它</h4>
                 </div>
-                <div class="col-2">
-                  <IncreaseNum start="0" end="15"></IncreaseNum>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-4 col-md-6 offset-lg-6 offset-md-2 project">
+                <div>
+                  <img class="projectPic" src="img/home/graphic.svg" alt="" />
                 </div>
-                <div class="col-9">
-                  <h4>projects can prove my ability</h4>
-                  <h4 class="chinese">个项目可以证明我</h4>
-                </div>
-                <div class="link">
-                  <SquareLink link="work#creative">take a look</SquareLink>
+                <div class="row substitle">
+                  <div class="col-1">
+                    <h4>These</h4>
+                    <h4 class="chinese">这</h4>
+                  </div>
+                  <div class="col-2">
+                    <IncreaseNum start="0" end="15"></IncreaseNum>
+                  </div>
+                  <div class="col-9">
+                    <h4>projects can prove my ability</h4>
+                    <h4 class="chinese">个项目可以证明我</h4>
+                  </div>
+                  <div class="link">
+                    <SquareLink link="work#creative">take a look</SquareLink>
+                  </div>
                 </div>
               </div>
             </div>
@@ -214,7 +220,11 @@ export default {
         keyboard: {
           enabled: true,
           onlyInViewport: false
-        }
+        },
+        lazy: {
+          loadPrevNext: true
+        },
+        preloadImages: false
       }
     };
   },
@@ -229,15 +239,18 @@ export default {
 </script>
 <style lang="scss">
 @import '../assets/scss/now-ui-kit/variables.scss';
-
 .swiper-container {
   height: 100vh;
   width: 100vw;
 
-  .swiper-slide {
+  .swiper-slide,
+  .bg-img {
     background-position: center center;
     background-size: cover;
-    background-color: white;
+    height: 100vh;
+    width: 100vw;
+  }
+  .bg-img {
     padding: 70px 5%;
   }
 
