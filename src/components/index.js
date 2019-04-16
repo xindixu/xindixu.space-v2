dcebcdd6e76894269b050d200cb8d56c9fe47048
Xindi Xu <xindixu@utexas.edu>
2019-04-16 15:11:00 -0500

    remove unused components

--- src/components/index.js
+++ src/components/index.js
@@ -4,27 +4,27 @@ import Radio from './Inputs/Radio.vue';
 import FormGroupInput from './Inputs/formGroupInput.vue';
 import Switch from './Switch.vue';
 import Badge from './Badge';
-import Progress from './Progress.vue';
+// import Progress from './Progress.vue';
 import Button from './Button.vue';
-import DropDown from './Dropdown.vue';
+// import DropDown from './Dropdown.vue';
 import Card from './Cards/Card.vue';
-import Collapse from './Collapse/Collapse.vue';
-import CollapseItem from './Collapse/CollapseItem.vue';
+// import Collapse from './Collapse/Collapse.vue';
+// import CollapseItem from './Collapse/CollapseItem.vue';
 import Navbar from './Navbar/Navbar';
 import NavLink from './Navbar/NavLink';
 import NavbarToggleButton from './Navbar/NavbarToggleButton';
 import TabPane from './Tabs/TabPane.vue';
 import Tabs from './Tabs/Tabs.vue';
 import Modal from './Modal.vue';
-import Parallax from './Parallax.vue';
-import Pagination from './Pagination.vue';
+// import Parallax from './Parallax.vue';
+// import Pagination from './Pagination.vue';
 
 // own components
 import QuickMenu from './QuickMenu.vue';
 import SquareLink from './SquareLink.vue';
 import SquareTexts from './SquareTexts.vue';
 import IncreaseNum from './IncreaseNum.vue';
-import CurveText from './CurveText.vue';
+// import CurveText from './CurveText.vue';
 import TableOfContent from './TableOfContent.vue';
 import SocialSideBar from './SocialSideBar.vue';
 import Editor from './Comment/Editor.vue';
@@ -32,40 +32,40 @@ import Message from './Comment/Message.vue';
 import CommentSection from './Comment/CommentSection.vue';
 import WorkInProgress from './WorkInProgress.vue';
 import ScrollDown from './ScrollDown.vue';
-import BlurLazyImage from './BlurLazyImage.vue';
+// import BlurLazyImage from './BlurLazyImage.vue';
 
 export {
   Alert,
-  Collapse,
-  CollapseItem,
+  // Collapse,
+  // CollapseItem,
   Checkbox,
   Switch,
   Badge,
-  Progress,
-  Pagination,
+  // Progress,
+  // Pagination,
   Radio,
   FormGroupInput,
   Card,
-  DropDown,
+  // DropDown,
   Navbar,
   NavbarToggleButton,
   NavLink,
   TabPane,
   Tabs,
   Modal,
-  Parallax,
+  // Parallax,
   Button,
   QuickMenu,
   SquareLink,
   SquareTexts,
   IncreaseNum,
-  CurveText,
+  // CurveText,
   TableOfContent,
   SocialSideBar,
   WorkInProgress,
   ScrollDown,
   Editor,
   Message,
-  CommentSection,
-  BlurLazyImage
+  CommentSection
+  // BlurLazyImage
 };
